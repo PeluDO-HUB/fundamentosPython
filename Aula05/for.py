a40d71e90c5532d55dd4ea02895481361cac9be2
jihye <jihye83@hotmail.com>
2026-03-27 16:49:01 -0300

    Exercicios utilizando While

---
+++ Aula05/for.py
@@ -0,0 +1,119 @@
+
+import os
+def menuPrograma():
+    print("Escolha qual Exercicio quer ver\nDigite 1 a 5 para os exercicios, 6 limpará o prompt de comando e 7 termina o programa")
+    escolha=int(input("Esperando o numero do Exercicio...   "))
+    match escolha:
+        case 1:
+            sexo()
+        case 2:
+            numPar()
+        case 3:
+            media()
+        case 4:
+            senha()
+        case 5:
+            fibonachi()
+        case 6:
+            os.system("cls")
+            menuPrograma()
+        case 7:
+           print("Finalizando o programa...")
+        case _:
+            print("opção invalida")
+            menuPrograma()
+
+"""
+1. Desenvolva
+um programa que funcione da seguinte maneira: Solicite ao usuário que informe a
+quantidade total de pessoas em um grupo. Para cada pessoa, peça que o
+usuário registre o sexo, utilizando a letra 'm' ou 'M' para masculino. O programa deve contar e, ao final,
+exibir o total de pessoas do sexo masculino.
+"""
+def sexo():
+    qtdPessoas= int(input("Digite a quantidade de pessoas: "))
+    masc = 0
+    fem = 0
+    for pessoa in range(0,qtdPessoas,1):
+        genero = input(f"Insira o genero da {pessoa+1}° pessoa\nM para Masculino e F para feminino ").upper()
+        if genero == "M" or genero=="MASCULINO":
+            masc+=1
+            print("Homem registrado!")
+        elif genero =="F" or genero=="FEMININO":
+            fem+=1
+            print("Mulher registrada")
+    else:
+        print(f"Foram adicionadas {qtdPessoas}, {masc} eram Homens e {fem} eram Mulheres")
+
+    menuPrograma()
+
+
+
+"""
+2. Desenvolva
+um programa em Python que utilize os comandos aprendidos para identificar e
+exibir todos os números pares entre 1 e 100.
+"""
+
+def numPar():
+    for i in range(0,100,1):
+        if i%2==0:
+            print(i)
+        else:
+            continue
+    menuPrograma()
+
+"""
+3. Crie
+um programa que leia 5 números e calcule a soma e a média desses números,
+exibindo os resultados.
+"""
+def media():
+    soma = 0
+    for i in range(1,5,1):
+        soma+= int(input(f"Digite o {i}° Numero a ser adicionado: "))
+        print(soma)
+    else:
+        print(f"{soma} / 5 = {soma/i}")
+    menuPrograma()
+
+
+
+"""
+4. Desenvolva um programa que solicite ao usuário um nome e uma senha,
+garantindo que a senha não seja igual ao nome de usuário. Se forem iguais,
+exiba uma mensagem de erro e peça as informações novamente.
+"""
+def senha():
+    for i in range(0,5,1):
+        nome = input("Digite seu nome de Usuario:")
+        senha = input("Digite sua senha: ")
+        if senha == nome:
+            print("Nome de Usuario e senha Iguais, tente novamente!")
+            continue
+        else:
+            print("Cadastro Concluido!")
+            break
+    else:
+        print("Cadastro não concluido!")
+    menuPrograma()
+
+
+def fibonachi():
+    fibo=0
+    for i in range(0,6,fibo+1):
+        if i == 0 :
+            print(i)
+            i+=1
+            fibo = i
+        elif i ==1:
+            print(i)
+            print(i)
+            i+=1
+            fibo = i
+        else:
+            print(fibo)
+            fibo+=i-1
+
+
+menuPrograma()
